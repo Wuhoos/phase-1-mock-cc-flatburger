fe8cc2265513d20ff89b9cd3a62663ff4f2131c1
David <wjw3298@gmail.com>
2023-08-09 11:31:45 -0400

    complete Mock CC

--- src/index.js
+++ src/index.js
@@ -1,1 +1,40 @@
-// Your code here
+const burgerMenu = document.querySelector('#restaurant-menu')
+const inputForm = document.querySelector('#add-to-cart-form')
+const foodDeatil = document.querySelector('#food-detail')
+const image = document.querySelector('#image')
+const burgerName = document.querySelector('#name')
+const numberInCart = document.querySelector('#number-in-cart-count')
+
+
+
+fetch('http://localhost:3000/burgers')
+.then(response => response.json())
+.then(burgerList => {
+    showBurgerName(burgerList)
+    showBurgerDetails(burgerList[0])
+    addingBurgers()
+})
+
+function showBurgerName(burgerList){
+    burgerList.forEach(burger => {
+        const burgerSpan = document.createElement('span')
+        burgerSpan.textContent = burger.name
+        burgerMenu.appendChild(burgerSpan)
+        burgerSpan.addEventListener('click', () => showBurgerDetails(burger))
+    })
+}
+
+function showBurgerDetails(burger){
+    numberInCart.textContent = burger.number_in_cart
+    image.src = burger.image
+    burgerName.textContent = burger.name
+}
+
+
+function addingBurgers(){
+    inputForm.addEventListener('submit', (e) => {
+        e.preventDefault()
+        const numberToAdd = document.getElementById('number-to-add').value
+        numberInCart.textContent = parseInt(numberInCart.textContent) + parseInt(numberToAdd)
+    })
+}
